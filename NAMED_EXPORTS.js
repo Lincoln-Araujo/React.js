6a8101b857eeb698a02c7f97a7520712dedf2d69
Lincoln Araujo <72479081+Lincoln-Araujo@users.noreply.github.com>
2022-01-05 15:27:43 -0300

    Create NAMED_EXPORTS.js
    how to export a class in React.js

---
+++ NAMED_EXPORTS.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// putting the word export before a class you already can export that class
+export class NavBar extends React.Component {
+  render() {
+   
+    //your logic here
+    
+    // don't forget to return what you expect your component should do
+    return // somehting;
+  }
+}
